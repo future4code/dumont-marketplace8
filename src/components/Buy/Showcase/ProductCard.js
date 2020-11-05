beedd346d41ea505710b026d44fcb6dc3e0823dd
Marina Azzi Nogueira <marinaazzinogueira@Marinas-MacBook-Pro.local>
2020-11-05 15:25:52 -0300

    vitrine e card de produtos

--- src/components/Buy/Showcase/ProductCard.js
+++ src/components/Buy/Showcase/ProductCard.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+    border: 1px dotted #F2C063;
+    text-align: center;
+`
+
+const ProductPicture = styled.img`
+    max-width: 300px;
+    max-height: 300px;
+    width: auto;
+    height: auto;
+    margin-left: auto;
+    margin-right: auto;
+`
+
+const CardInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 16px;
+
+    p{
+        margin: 4px;
+    }
+`
 
 class ProductCard extends React.Component {
     render() {
         return (
-            <div>ESTE É O COMPONENTE DE PRODUCT CARD</div>
+            <CardContainer>
+                <ProductPicture src= 'https://picsum.photos/100/150'/>
+                <CardInfo>
+                    <p>Nome</p>
+                    <p>Descrição</p>
+                    <p>Categoria</p>
+                    <p>Preço</p>
+                    <p>Método de Pagamento</p>
+                    <p>Parcelamento</p>
+                    <Button
+                        variant='contained'
+                        color= 'primary'
+                        size='medium'
+                        startIcon={<AddShoppingCartIcon />}
+                    >
+                        Adicionar ao carrinho!
+                    </Button>
+                </CardInfo>
+
+            </CardContainer>
         )
     }
 }
