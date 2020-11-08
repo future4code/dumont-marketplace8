658d2b6835b614ae86a13e20f09c749732a110ce
fefealfonsi <fefealfonsi@gmail.com>
2020-11-08 16:13:52 -0300

    consertando os erros do carrinho, na funcao addProdutoCarrinho

--- src/components/Buy/Buy.js
+++ src/components/Buy/Buy.js
@@ -20,79 +20,77 @@ const products = [
     {
         name: "produto 1 TESTE",
         description:"produtãovish",
-        price:"10",
+        price:10,
         paymentMethod:"dinheiro",
         category:"roupa",
         photos:['https://picsum.photos/200/?a=1', 'https://picsum.photos/200/?a=2'],
-        installments:"3",
-        id:"1"
+        installments:3,
+        id:1
     },
     {
         name: "produto 2",
         description:"produtãovish2",
-        price:"102",
+        price:102,
         paymentMethod:"cartao",
         category:"movel",
         photos:['https://picsum.photos/200/?a=3', 'https://picsum.photos/200/?a=4'],
-        installments:"3",
-        id:"2"
+        installments:3,
+        id:2
     },
     {   
         name: "produto 3",
         description:"produtãovish3",
-        price:"103",
+        price:103,
         paymentMethod:"cheque",
         category:"acessorio",
         photos:['https://picsum.photos/200/?a=5', 'https://picsum.photos/200/?a=6'],
-        installments:"6",
-        id:"3"
+        installments:6,
+        id:3
     },
     {
         name: "produto 4",
         description:"produtãovish4",
-        price:"104",
+        price:104,
         paymentMethod:"dinheiro",
         category:"roupa",
         photos:['https://picsum.photos/200/?a=7', 'https://picsum.photos/200/?a=8'],
-        installments:"3",
-        id:"4"
+        installments:3,
+        id:4
     },
     {
         name: "produto 5",
         description:"produtãovish",
-        price:"10",
+        price:10,
         paymentMethod:"dinheiro",
         category:"roupa",
         photos:['https://picsum.photos/200/?a=9', 'https://picsum.photos/200/?a=10'],
-        installments:"3",
-        id:"5"
+        installments:3,
+        id:5
     },
     {
         name: "produto 6",
         description:"produtãovish6",
-        price:"10",
+        price:10,
         paymentMethod:"dinheiro",
         category:"roupa",
         photos:['https://picsum.photos/200/?a=11', 'https://picsum.photos/200/?a=12'],
-        installments:"3",
-        id:"6"
+        installments:3,
+        id:6
     },
     {
         name: "produto 7",
         description:"produtãovish",
-        price:"10",
+        price:10,
         paymentMethod:"dinheiro",
         category:"roupa",
         photos:['https://picsum.photos/200/?a=13', 'https://picsum.photos/200/?a=14'],
-        installments:"3",
-        id:"7"
+        installments:3,
+        id:7
     }
 ]
 
 class Buy extends React.Component {
     state = {
-
-      
 
         listaDoCarrinho:[    ]
      }
@@ -112,86 +110,63 @@ class Buy extends React.Component {
          if(produtoNoCarrinho){
              const novoProdutoNoCar = this.state.listaDoCarrinho.map((produto)=>{
                  if(produtoId===produto.id){
-                     return {...produto, quantidade:produto.quantidade + 1}
+                     return {...produto, quantity:produto.quantity+1}
                  }
                  return produto
              })
              this.setState({listaDoCarrinho:novoProdutoNoCar})
          }else{
-             const produtoParaAdd = ArrayDeProdutos.find((produto)=>produtoId===produto.id)
-             const novoProdutoNoCar = [...this.state.listaDoCarrinho,{...produtoParaAdd,quantidade:1}]
+             const produtoParaAdd = products.find((produto)=>produtoId===produto.id)
+             const novoProdutoNoCar = [...this.state.listaDoCarrinho,{...produtoParaAdd,quantity:1}]
              this.setState({listaDoCarrinho: novoProdutoNoCar})
          }
+
+         console.log(products)
      }
+
+     
+
      removeProdutoCarrinho = (produtoId)=>{
          const novoProdutoNoCar = this.state.listaDoCarrinho.map((produto)=>{
              if(produto.id ===produtoId){
-                 return {...produto,quantidade: produto.quantidade-1 }
+                 return {...produto,quantity: produto.quantity-1 }
              }
              return produto
-         }).filter((produto)=>produto.quantidade>0)
+         }).filter((produto)=>produto.quantity>0)
          this.setState({listaDoCarrinho:novoProdutoNoCar})
      }
  
-     
+        
  
-        productsInCart: [{
-            name: "produto 1 TESTE",
-            description:"produtãovish",
-            price:"10",
-            paymentMethod:"dinheiro",
-            category:"roupa",
-            photos:['https://picsum.photos/200/?a=1', 'https://picsum.photos/200/?a=2'],
-            installments:"3",
-            id:"1"
-        },
-        {
-            name: "produto 2",
-            description:"produtãovish2",
-            price:"102",
-            paymentMethod:"cartao",
-            category:"movel",
-            photos:['https://picsum.photos/200/?a=3', 'https://picsum.photos/200/?a=4'],
-            installments:"3",
-            id:"2"
-        },
-        {   
-            name: "produto 3",
-            description:"produtãovish3",
-            price:"103",
-            paymentMethod:"cheque",
-            category:"acessorio",
-            photos:['https://picsum.photos/200/?a=5', 'https://picsum.photos/200/?a=6'],
-            installments:"6",
-            id:"3"
-        },]
-    }
-    onAddProductToCart = (productId) => {
-        const productInCart = this.state.productsInCart.find(product => productId === product.id)
+    
+  
+    // onAddProductToCart = (productId) => {
+    //     const productInCart = this.state.productsInCart.find(product => productId === product.id)
 
-        if(productInCart) {
-            const newProductsInCart = this.state.productsInCart.map(product => {
-                if(productId === product.id) {
-                    return {
-                        ...product,
-                        quantity: product.quantity + 1
-                    }
-                }
+    //     if(productInCart) {
+    //         const newProductsInCart = this.state.productsInCart.map(product => {
+    //             if(productId === product.id) {
+    //                 return {
+    //                     ...product,
+    //                     quantity: product.quantity + 1
+    //                 }
+    //             }
 
-                return product
-            })
+    //             return product
+    //         })
 
-            this.setState({ productsInCart: newProductsInCart})
-        } else {
-            const productToAdd = products.find(product => productId === product.id)
+    //         this.setState({ productsInCart: newProductsInCart})
+    //     } else {
+    //         const productToAdd = products.find(product => productId === product.id)
 
-            const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1}]
+    //         const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1}]
 
-            this.setState({ productsInCart: newProductsInCart })
-        }
-    }
+    //         this.setState({ productsInCart: newProductsInCart })
+    //     }
+    // }
 
     render() {
+        
         return (
             <BuyContainer>
                 <MenuSuperior shopButton ={this.props.shopButton} />
@@ -199,19 +174,13 @@ class Buy extends React.Component {
                     <Filters />
 
                     <Showcase
+                        products={products} 
                         addProdutoCarrinho={this.addProdutoCarrinho}
                      />
                     <ShoppingCart 
-                        adicionaCarrinho={this.state.listaDoCarrinho}
+                        listaDoCarrinho={this.state.listaDoCarrinho}
                         removeProdutoCarrinho={this.removeProdutoCarrinho}
                     />
-
-                    <Showcase 
-                        products={products} 
-                        onAddProductToCart={this.onAddProductToCart}
-                    />
-                    <ShoppingCart />
-
                 </SectionsGrid>
             </BuyContainer>
         )
